ab567258738a64aea89eae11cb9faff0890d7a2a
Alex Omand <aomand@hotmail.co.uk>
2024-06-16 15:11:26 +0100

    turtle race

--- day-19-start/main.py
+++ day-19-start/main.py
@@ -1,36 +1,36 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
 screen = Screen()
-
-def move_forwards():
-    tim.forward(10)
-
-
-def move_backwards():
-    tim.back(10)
-
-
-def turn_left():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a colour: ")
+colours = ["red", "gold", "pink", "green", "orange", "blue"]
+all_turtles = []
+starting_y = -90
+is_race_on = False
 
 
-def turn_right():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colours[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=starting_y)
+    starting_y += 30
+    all_turtles.append(new_turtle)
 
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_colour = turtle.pencolor()
+            if winning_colour == user_bet:
+                print(f"You've won! The {winning_colour} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_colour} turtle is the winner!")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
-screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards)
-screen.onkey(key="a", fun=turn_left)
-screen.onkey(key="d", fun=turn_right)
-screen.onkey(key="space", fun=clear)
 screen.exitonclick()
