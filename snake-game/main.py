cdeb50d1dc9e7869dd4066722a8b7024c4a72cfd
Alex <aomand@hotmail.co.uk>
2024-06-18 14:33:20 +0100

    added food to snake game

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
-import time
+from turtle import Screen
 from snake import Snake
+from food import Food
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,13 @@ screen.title("snake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
@@ -16,5 +24,8 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    #Detect collision with food.
+    if snake.snake_head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
