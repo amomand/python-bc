1942bc3964087e13de3eb30ad0a839550adff385
Alex <aomand@hotmail.co.uk>
2024-06-25 14:07:58 +0100

    player behaviour

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -5,6 +5,7 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+protagonist = Player()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
@@ -12,3 +13,11 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    screen.listen()
+    screen.onkey(protagonist.move, "Up")
+
+    #Detect when at finish line.
+    if protagonist.at_finish_line:
+        print("You made it!")
+        game_is_on = False
+
--- turtle-crossing/player.py
+++ turtle-crossing/player.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("dark green")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        self.at_finish_line = False
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
+        if self.ycor() == 280:
+            self.at_finish_line = True
