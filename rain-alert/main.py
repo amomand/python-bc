640f6b9dbf3293b3427ac6c84152e489477034cb
Alex <aomand@hotmail.co.uk>
2025-01-14 16:49:15 +0000

    added rain chack logic to rain-alert

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -6,9 +6,18 @@ OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 parameters = {
     "lat": 51.72,
     "lon": -0.83,
-    "appid": api_key
+    "appid": api_key,
+    "cnt": 4
 }
 
-weather_response = requests.get(OWM_endpoint, params=parameters)
-weather_response.raise_for_status()
-print(weather_response.json())
+response = requests.get(OWM_endpoint, params=parameters)
+response.raise_for_status()
+weather_data=response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = (hour_data["weather"][0]["id"])
+    if int(condition_code) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella!")
